e9cfbbdd77405c9e7b694b317d93666e2fd54021
MohamedRT <motifyee@gmail.com>
2023-08-26 05:52:44 +0300

    chore: cleanup and refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,98 +1,55 @@
 import './App.css';
-import { jsPDF } from 'jspdf';
-
-function pixelsToMM(elId) {
-	const el = document.getElementById(elId);
-	el.classList.add('printing');
-	const width = parseInt(window.getComputedStyle(el, null).width);
-	const height = parseInt(window.getComputedStyle(el, null).height);
-	const converted = {};
-
-	converted['width'] = Math.floor(width * 0.264583);
-	converted['height'] = Math.floor(height * 0.264583);
-
-	console.log(converted.width + ' ' + converted.height);
-
-	return [el, converted.width, converted.height];
-}
+import {
+	canvas2blob,
+	canvas2pdf,
+	el2canvas,
+	el2pdf,
+	saveCanvasAsImage,
+	send2printer,
+} from './scr';
 
 function App() {
-	const url = 'http://localhost:3030/file';
+	async function save() {
+		// let el = document.getElementById('printable')?.cloneNode(true);
 
-	function save() {
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const [el, _, h] = pixelsToMM('root');
-		// console.log(w, h);
-		const doc = jsPDF('p', 'mm', [80, h]);
-		doc.html(el, {
-			callback: doc => {
-				_fetch(doc);
-				el.classList.remove('printing');
-			},
-			x: 0,
-			y: 0,
-			width: 80,
-			windowWidth: 368,
-		});
+		// console.log('run 2');
+		// console.log(el);
+		// document.getElementById('printable')?.remove();
+
+		const canvas = await el2canvas('printable');
+		// saveCanvasAsImage(canvas);
+		// let blob = await canvas2blob(canvas);
+		// send2printer(blob);
+		canvas2pdf(canvas, true);
+		// el2pdf('printable', true);
 	}
-	function _fetch(doc) {
-		doc.save('test.pdf');
-		const data = new FormData();
-		data.append('fl', doc.output('blob'));
-		fetch(url, {
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-				'Access-Control-Allow-Credentials': false,
-			},
-			method: 'POST',
-			body: data,
-			mode: 'no-cors',
-		});
-	}
+
 	return (
 		<>
-			<h2>HTML Table</h2>
+			<div className='container'>
+				<div id='printable'>
+					<h2>بسم الله الرحمن الرحيم</h2>
 
-			<table>
-				<tbody>
-					<tr>
-						<th>Company</th>
-						<th>Contact</th>
-						<th>Country</th>
-					</tr>
-					<tr>
-						<td>Alfreds Futterkiste</td>
-						<td>Maria Anders</td>
-						<td>Germany</td>
-					</tr>
-					<tr>
-						<td>Centro comercial Moctezuma</td>
-						<td>Francisco Chang</td>
-						<td>Mexico</td>
-					</tr>
-					<tr>
-						<td>Ernst Handel</td>
-						<td>Roland Mendel</td>
-						<td>Austria</td>
-					</tr>
-					<tr>
-						<td>Island Trading</td>
-						<td>Helen Bennett</td>
-						<td>UK</td>
-					</tr>
-					<tr>
-						<td>Laughing Bacchus Winecellars</td>
-						<td>Yoshi Tannamuri</td>
-						<td>Canada</td>
-					</tr>
-					<tr>
-						<td>Magazzini Alimentari Riuniti</td>
-						<td>Giovanni Rovelli</td>
-						<td>Italy</td>
-					</tr>
-				</tbody>
-			</table>
-
+					<table>
+						<tbody>
+							<tr>
+								<th>n</th>
+								<th>Company</th>
+								<th>Contact</th>
+							</tr>
+							{Array(130)
+								.fill(0)
+								.map((_, e) => (
+									<tr key={e}>
+										<td>{e}</td>
+										<td>Alfreds Futterkiste</td>
+										<td>Maria Anders</td>
+									</tr>
+								))}
+						</tbody>
+					</table>
+				</div>
+			</div>
 			<button onClick={save}>save pdf</button>
 		</>
 	);
---
+++ src/scr.ts
@@ -0,0 +1,145 @@
+import h2c from 'html2canvas';
+import { jsPDF } from 'jspdf';
+import setFont from './tj-bold';
+
+const PXTOMM = 0.2645833333;
+
+function _getElement(el: string | HTMLElement): HTMLElement | null {
+	let _el: HTMLElement | null =
+		typeof el === 'string' ? document.getElementById(el) : el;
+	return _el;
+}
+
+export function measureMM(
+	el: string | HTMLElement
+): [HTMLElement, number, number] {
+	const _el = _getElement(el),
+		[w, h] = ['width', 'height']
+			.map(prop => window.getComputedStyle(_el as HTMLElement, null)[prop])
+			.map(e => Math.floor(parseInt(e) * 0.264583));
+
+	return [_el as HTMLElement, w, h];
+}
+
+export function el2canvas(
+	el: string | HTMLElement
+): Promise<HTMLCanvasElement> {
+	const _el = _getElement(el);
+	return new Promise(resolve => {
+		h2c(_el as HTMLElement, {
+			onclone: clonedDoc => {
+				clonedDoc.documentElement.style.display = 'block';
+			},
+		}).then(canvas => {
+			resolve(canvas);
+		});
+	});
+}
+
+export function saveCanvasAsImage(canvas: HTMLCanvasElement) {
+	let uri = canvas
+		.toDataURL('image/png')
+		.replace('image/png', 'image/octet-stream');
+	window.location.href = uri;
+
+	// let blob = await canvas2blob(canvas);
+	// let link = document.createElement('a');
+	// link.href = URL.createObjectURL(blob ?? new Blob());
+	// console.log(blob);
+	// console.log(link.href);
+	// link.click();
+}
+
+export function canvas2blob(canvas: HTMLCanvasElement): Promise<Blob> {
+	return new Promise(resolve => {
+		canvas.toBlob(blob => resolve(blob ?? new Blob()), 'image/png');
+	});
+}
+
+export function canvas2pdf(canvas: HTMLCanvasElement, save = false) {
+	let w = canvas.width * PXTOMM,
+		h = canvas.height * PXTOMM;
+	// uri = canvas.toDataURL('image/png');
+
+	const doc = new jsPDF('p', 'mm', [w, h]);
+	doc.addImage(canvas, 'PNG', 0, 0, w, h);
+	if (save) doc.save('test.pdf');
+	return doc;
+}
+
+export function jsPDF2blob(jsPDF: jsPDF) {
+	return jsPDF.output('blob');
+}
+
+export async function el2pdf(
+	el: string | HTMLElement,
+	save = false
+): Promise<jsPDF> {
+	const _el = _getElement(el),
+		doc = new jsPDF({
+			orientation: 'p',
+			unit: 'mm',
+			format: [80, 130],
+		});
+	// doc = new jsPDF('p', 'mm', [80, 130]);
+
+	setFont(doc);
+	doc.setFont('Tajawal-Bold', 'normal');
+	doc.table(
+		3,
+		5,
+		Array(30)
+			.fill(0)
+			.map(() => ({
+				a: 'string',
+				b: 'b-string',
+				c: 'b-string which is some what is long',
+			})),
+		['a', 'b', 'c'],
+		{
+			padding: 3,
+			autoSize: true,
+			rowStart: (e, doc) => {
+				console.log(e);
+				// if (e.row == 2)
+				// 	doc.setTableHeaderRow([
+				// 		{ width: 23, align: 'center', name: '32', padding: 0, prompt: '' },
+				// 	]);
+			},
+			cellStart(e, doc) {},
+			printHeaders: true,
+		}
+	);
+	if (save) doc.save('download-file');
+	// return new Promise(resolve => {
+	// 	doc.html(_el as HTMLElement, {
+	// 		html2canvas: {
+	// 			width: 302,
+	// 		},
+	// 		callback: doc => {
+	// 			// print(doc);
+	// 			if (save) doc.save('download-file');
+	// 			resolve(doc);
+	// 			// (el as HTMLElement)?.classList.remove('printing');
+	// 		},
+	// 		x: 0,
+	// 		y: 0,
+	// 		width: 80,
+	// 		// width: 80, //target width in the PDF document
+	// 		windowWidth: 300, //window width in CSS pixels
+	// 	});
+	// });
+}
+
+export function send2printer(blob: Blob) {
+	const url = 'http://localhost:3030/file';
+
+	const data = new FormData();
+	data.append('fl', blob);
+
+	fetch(url, {
+		method: 'POST',
+		body: data,
+		mode: 'no-cors',
+	});
+}
